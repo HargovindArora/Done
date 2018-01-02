17c0c6bf79df4bb92686a69e8b48e9a530f926fc
HargovindArora <hargovinds50@gmail.com>
2018-01-02 16:50:31 +0530

    Views and unittests updated

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from lists.models import Item
+from lists.models import Item, List
 # Create your views here.
 def home_page(request):
     return render(request, 'home.html')
@@ -9,5 +9,6 @@ def view_list(request):
     return render(request, 'list.html', {'items': items})
 
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/the-only-list-in-the-world/')

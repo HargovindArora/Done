1b0ce268f3a27b68ca1b6920033bc8e58a1a004a
HargovindArora <hargovinds50@gmail.com>
2018-01-16 11:35:43 +0530

    Moved FTs into their own individual files

---
+++ functional_tests/test_list_item_validation.py
@@ -0,0 +1,11 @@
+from .base import FunctionalTest
+from selenium.webdriver.common.keys import Keys
+from unittest import skip
+
+
+class ItemValidationTest(FunctionalTest):
+
+
+    def test_cannot_add_empty_list_items(self):
+
+        self.fail('write me!')

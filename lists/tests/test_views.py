f4c50f231434b051564e985569d9e40cabb15a94
HargovindArora <hargovinds50@gmail.com>
2018-01-18 16:54:28 +0530

    Adjust new list view to do model validation

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from lists.models import Item, List
-
+from django.utils.html import escape
 
 class HomePageTest(TestCase):
 
@@ -25,6 +25,18 @@ class NewListTest(TestCase):
         new_list = List.objects.first()
         self.assertRedirects(response, f'/lists/{new_list.id}/')
 
+
+    def test_validation_errors_are_sent_back_to_home_page_template(self):
+        response = self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+        expected_error = escape("You can't have an empty list item")
+        self.assertContains(response, expected_error)
+
+    def test_valid_list_items_arent_saved(self):
+        self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(List.objects.count(), 0)
+        self.assertEqual(Item.objects.count(), 0)
 
 
 class NewItemTest(TestCase):
--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from lists.models import Item, List
+from django.core.exceptions import ValidationError
+
 # Create your views here.
+
 def home_page(request):
     return render(request, 'home.html')
 
@@ -10,7 +13,14 @@ def view_list(request, list_id):
 
 def new_list(request):
     list_ = List.objects.create()
-    Item.objects.create(text=request.POST['item_text'], list=list_)
+    item = Item(text=request.POST['item_text'], list=list_)
+    try:
+        item.full_clean()
+        item.save()
+    except ValidationError:
+        list_.delete()
+        error = "You can't have an empty list item"
+        return render(request, 'home.html', {"error": error})
     return redirect(f'/lists/{list_.id}/')
 
 def add_item(request, list_id):

84887a6fe4fb92d78d440ec2fedf9082e820c2fc
HargovindArora <hargovinds50@gmail.com>
2018-01-30 00:25:44 +0530

    Use get_absolute_url on list model to DRY urls in views

--- lists/models.py
+++ lists/models.py
@@ -1,8 +1,10 @@
 from django.db import models
-
+from django.core.urlresolvers import reverse
 # Create your models here.
 class List(models.Model):
-    pass
+
+    def get_absolute_url(self):
+        return reverse('view_list', args=[self.id])
 
 class Item(models.Model):
     text = models.TextField(default='')

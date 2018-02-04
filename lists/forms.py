69401841ac26b5d81352595f7ac673c312623b5a
HargovindArora <hargovinds50@gmail.com>
2018-02-04 23:44:29 +0530

    Added custom save method for saving input to forms

--- lists/forms.py
+++ lists/forms.py
@@ -5,15 +5,19 @@ EMPTY_ITEM_ERROR = "You can't have an empty list item"
 
 class ItemForm(forms.models.ModelForm):
 
-        class Meta:
-            model = Item
-            fields = ('text',)
-            widgets = {
-                'text': forms.fields.TextInput(attrs={
-                    'placeholder': 'Enter a to-do item',
-                    'class': 'form-control input-lg',
-                }),
-            }
-            error_messages = {
-                'text': {'required': EMPTY_ITEM_ERROR}
-            }
+    class Meta:
+        model = Item
+        fields = ('text',)
+        widgets = {
+            'text': forms.fields.TextInput(attrs={
+                'placeholder': 'Enter a to-do item',
+                'class': 'form-control input-lg',
+            }),
+        }
+        error_messages = {
+            'text': {'required': EMPTY_ITEM_ERROR}
+        }
+
+    def save(self, for_list):
+        self.instance.list = for_list
+        return super().save()

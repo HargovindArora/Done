77c31f88a0dfcdd7b2e1ab976cc1b337d4e62375
HargovindArora <hargovinds50@gmail.com>
2018-02-21 01:10:43 +0530

    Added unique_item Validation. Tests passing

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -23,6 +23,12 @@ class ItemFormTest(TestCase):
         self.assertEqual(new_item.text, 'do me')
         self.assertEqual(new_item.list, list_)
 
+    def test_form_save(self):
+        list_ = List.objects.create()
+        form = ExistingListItemForm(for_list=list_, data={'text': 'hi'})
+        new_item = form.save()
+        self.assertEqual(new_item, Item.objects.all()[0])
+
 class ExistingListItemFormTest(TestCase):
 
     def test_form_renders_item_text_input(self):
@@ -30,7 +36,7 @@ class ExistingListItemFormTest(TestCase):
         form = ExistingListItemForm(for_list=list_)
         self.assertIn('placeholder="Enter a to-do item"', form.as_p())
 
-    def test_form_validation_for_blank_items(TestCase):
+    def test_form_validation_for_blank_items(self):
         list_ = List.objects.create()
         form = ExistingListItemForm(for_list=list_, data={'text': ''})
         self.assertFalse(form.is_valid())
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,7 +1,10 @@
 from django.test import TestCase
 from lists.models import Item, List
 from django.utils.html import escape
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from lists.forms import (
+            ItemForm, EMPTY_ITEM_ERROR,
+            DUPLICATE_ITEM_ERROR, ExistingListItemForm,
+        )
 from unittest import skip
 
 class HomePageTest(TestCase):
@@ -123,7 +126,7 @@ class ListViewTest(TestCase):
 
     def test_for_invalid_input_passes_form_to_template(self):
         response = self.post_invalid_input()
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
 
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
@@ -132,10 +135,9 @@ class ListViewTest(TestCase):
     def test_displays_item_form(self):
         list_ = List.objects.create()
         response = self.client.get(f'/lists/{list_.id}/')
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
         self.assertContains(response, 'name="text"')
 
-    @skip
     def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
         list1 = List.objects.create()
         item1 = Item.objects.create(list=list1, text='textey')
@@ -144,7 +146,7 @@ class ListViewTest(TestCase):
             data={'text': 'textey'}
         )
 
-        expected_error = escape("You've already got this in your list")
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
         self.assertContains(response, expected_error)
         self.assertTemplateUsed(response, 'list.html')
         self.assertEqual(Item.objects.all().count(), 1)
